1716d860c0be43efd8304f914cc57205f6d99385
duanjiefei <duanjiefei@skyworth.com>
2018-03-27 23:31:12 +0800

    常见的几种基本数据类型

--- Pycharm_Project/3_27/pycharm_in.py
+++ Pycharm_Project/3_27/pycharm_in.py
@@ -5,3 +5,14 @@ if "duan" in name:
     print("True")
 else:
     print("False")
+
+# False常见的几个运算符  计算规则为从前到后
+#   =     <      >     >=     <=    !=   <>(不等于)
+
+# num = 123
+# int 数字
+# str 字符串
+# list 列表
+# tuple 元祖
+# dict 字典
+# bool 布尔值
